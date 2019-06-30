13c4275f311be5ba2f0f3e5231eabb405c739ad2
Nitin Vohra <nitinvohra92@gmail.com>
2019-06-30 20:16:11 +0530

    todo app

---
+++ todo.js
@@ -0,0 +1,42 @@
+let inputBox = document.getElementById('id1')
+let list = document.getElementById('list')
+
+let counter=0;
+
+function dofunction() {
+   
+    if(inputBox.value=="")
+  window.alert("no task to add");
+    
+    let num = inputBox.value;
+    let str = "";
+    
+    
+     str+=`<li  onclick="this.style   ='text-decoration: line-through'" >${num}</li>`;
+    
+    list.innerHTML +=str;
+    
+    inputBox.value=''
+    
+}
+
+function deletelist(){
+    let items=list.getElementsByTagName("li");
+   let y=false;
+    for (let i = 0; i <items.length ; i++) {
+        const st=items[i].style.textDecoration;
+        
+        if( st =="line-through"){
+            
+            
+            items[i].parentNode.removeChild(items[i])
+            i=-1;
+            y=true;
+        }
+        
+    }
+    if(y==false)
+    window.alert("No task to remove")
+}
+
+
